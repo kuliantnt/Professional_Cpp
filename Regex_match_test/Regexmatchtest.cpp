22798d1383a4be1bcc1780d29a32cdf291d8f083
lianlian <kuliantnt@users.noreply.github.com>
2017-09-22 09:12:57 +0800

    2017年9月22日09:12:52

--- Regex_match_test/Regexmatchtest.cpp
+++ Regex_match_test/Regexmatchtest.cpp
@@ -49,8 +49,8 @@ int main(int argc, char* argv)
 		else
 			std::cout << " No comment found!" << std::endl;
 	}
-}
-*/
+
+
 
 	std::regex reg("[\\w]+");
 	while (true)
@@ -59,11 +59,65 @@ int main(int argc, char* argv)
 		std::string str;
 		if (!getline(std::cin, str) || str == "q")
 			break;
-		const std::sregex_iterator end;
-		for (std::sregex_iterator iter(cbegin(str), cend(str), reg); iter != end;
+		const std::sregex_token_iterator end;
+		for (std::sregex_token_iterator iter(cbegin(str), cend(str), reg); iter != end;
 			++iter)
 		{
-			std::cout << "\"" << (*iter)[0] << "\"" << std::endl;
+			std::cout << "\"" << *iter << "\"" << std::endl;
 		}
 	}
+
+	std::regex reg("^(\\d{4})/(0?[1-9]|1[0-2])/(0?[1-9]|[1-2][0-9]|3[0-1])$");
+	while (true)
+	{
+		std::cout << "Enter a data (yyyy/mm/dd) (q=quit): ";
+		std::string str;
+		if (!getline(std::cin, str) || str == "q")
+		{
+			break;
+		}
+		std::vector<int> indices{ 2,3 };
+		const std::sregex_token_iterator end;
+		for (std::sregex_token_iterator iter(cbegin(str), cend(str), reg, indices);
+			iter != end;
+			++iter)
+		{
+			std::cout << "\"" << *iter << "\"" << std::endl;
+		}
+	}
+
+
+	std::regex reg(R"(\s*[,;]\s*)");
+	while (true)
+	{
+		std::cout << "Enter a string to spilt on ',' and ';' (q=quit): ";
+		std::string str;
+		if (!getline(std::cin, str) || str == "q")
+			break;
+		const std::sregex_token_iterator end;
+		for (std::sregex_token_iterator iter(cbegin(str), cend(str), reg, -1);
+			iter != end; ++iter)
+			std::cout << "\"" << *iter << "\"" << std::endl;
+	}
+
+	const std::string str("<body><h1>header</h1><p>some text</p></header>");
+	std::regex r("<h1>(.*)</h1><p>(.*)</p>");
+	const std::string format("H1=$1 and P=$2");
+	std::string result = std::regex_replace(str, r, format,std::regex_constants::format_no_copy);
+	std::cout << "Original string: '" << str << "'" << std::endl;
+	std::cout << "New string: '" << result << "'" << std::endl;
+	system("pause");
+	*/
+	std::regex reg("([\\w]+|[\\d]+)");
+	const std::string format("$1\n");
+	while (true)
+	{
+		std::cout << "Enter a string to split over multiple lines(q=quit): ";
+		std::string str;
+		if (!getline(std::cin, str) | str == "q")
+			break;
+		std::cout << std::regex_replace(str, reg, format
+			,std::regex_constants::format_no_copy) << std::endl;
+	}
+
 }
